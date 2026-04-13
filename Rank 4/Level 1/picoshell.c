249e38aaa685c619f9410a4b56d5c1bba91c941f
Antor Habib Chowdhury <achowdhu@c3r3p5.hive.fi>
2026-04-13 19:49:11 +0300

    picoshell timeout issue solved

--- Rank 4/Level 1/picoshell.c
+++ Rank 4/Level 1/picoshell.c
@@ -4,56 +4,71 @@
 
 int picoshell(char **cmds[])
 {
-	int fd[2], prev = -1, i = 0;
+    int fd[2];
+    int prev = -1;
+    int i = 0;
+    pid_t pid;
 
-	if (!cmds)
-		return (1);
-	while (cmds[i])
-	{
-		if (cmds[i + 1] && pipe(fd) == -1)
-		{
-			if (prev != -1)
-				close(prev);
-			return (1);
-		}
-		pid_t pid = fork();
-		if (pid == -1)
-		{
-			if (cmds[i + 1])
-			{
-				close(fd[0]);
-				close(fd[1]);
-			}
-			if (prev != -1)
-				close(prev);
-			return (1);
-		}
-		if (pid == 0)
-		{
-			if (prev != -1)
-			{
-				dup2(prev, 0);
-				close(prev);
-			}
-			if (cmds[i + 1])
-			{
-				close(fd[0]);
-				dup2(fd[1], 1);
-				close(fd[1]);
-			}
-			execvp(cmds[i][0], cmds[i]);
-			exit(1);
-		}
-		if (prev != -1)
-			close(prev);
-		if (cmds[i + 1])
-		{
-			close(fd[1]);
-			prev = fd[0];
-		}
-		i++;
-	}
-	while (wait(NULL) != -1)
-		;
-	return (0);
+    if (!cmds || !cmds[0])
+        return (0);
+
+    while (cmds[i])
+    {
+        if (cmds[i + 1] && pipe(fd) == -1)
+        {
+            if (prev != -1)
+                close(prev);
+            return (1);
+        }
+
+        pid = fork();
+        if (pid == -1)
+        {
+            if (cmds[i + 1])
+            {
+                close(fd[0]);
+                close(fd[1]);
+            }
+            if (prev != -1)
+                close(prev);
+            return (1);
+        }
+
+        if (pid == 0)
+        {
+            if (prev != -1)
+            {
+                dup2(prev, 0);
+                close(prev);
+            }
+            if (cmds[i + 1])
+            {
+                close(fd[0]);
+                dup2(fd[1], 1);
+                close(fd[1]);
+            }
+            execvp(cmds[i][0], cmds[i]);
+            exit(1);
+        }
+
+        if (prev != -1)
+            close(prev);
+
+        if (cmds[i + 1])
+        {
+            close(fd[1]);
+            prev = fd[0];
+        }
+        else
+        {
+            prev = -1;
+        }
+        i++;
+    }
+
+    if (prev != -1)
+        close(prev);
+
+    while (wait(NULL) > 0);
+    return (0);
 }
